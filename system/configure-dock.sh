58dd7c29f07e437174d6558e0a34ffc1174ada4d
Cameron Yule <cameron@cameronyule.com>
2016-06-20 12:08:30 +0100

    Configuring Dock layout.

---
+++ system/configure-dock.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+dockutil --remove all
+
+dockutil --add /Applications/Google\ Chrome.app
+dockutil --add /Applications/AirMail\ Beta.app
+dockutil --add /Applications/Calendar.app
+
+dockutil --add /usr/local/Cellar/emacs/25.0.95/Emacs.app
+dockutil --add /Applications/Utilities/Terminal.app
+dockutil --add /Applications/Dash.app
+dockutil --add /Applications/Pomodoro\ One.app
+dockutil --add /Applications/Slack.app
+
+dockutil --add /Applications/OmniFocus.app
+dockutil --add /Applications/Evernote.app
+dockutil --add /Applications/1Password\ 6.app
+
+dockutil --add /Applications/Pocket.app
+
+dockutil --add /Applications/Spotify.app
+dockutil --add /Applications/Sonos.app
+
+dockutil --add '~/Documents' --view grid --display folder
+dockutil --add '~/Downloads' --view grid --display folder
