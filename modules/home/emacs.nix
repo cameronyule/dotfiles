b1a0fea5e524ec5b0b782e36fb1a9c7580df4c29
Cameron Yule <cameron@cameronyule.com>
2025-06-03 18:43:28 +0100

    Removing redundancy in theme configuration.

--- modules/home/emacs.nix
+++ modules/home/emacs.nix
@@ -380,7 +380,8 @@
           ;; There are two ways to load a theme. Both assume the theme is installed and
           ;; available. You can either set `doom-theme' or manually load a theme with the
           ;; `load-theme' function. This is the default:
-          (setq doom-theme 'doom-one)
+
+          (setq doom-theme 'doom-solarized-light)
 
           ;; This determines the style of line numbers in effect. If set to `nil', line
           ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -433,9 +434,6 @@
 
           (use-package! typst-ts-mode
            :mode "\\.typ\\'")
-
-          ;; Theme
-          (setq doom-theme 'doom-solarized-light)
           (setq doom-font (font-spec :family "FiraCode Nerd Font" :size 12 :weight 'medium))
 
         '';
